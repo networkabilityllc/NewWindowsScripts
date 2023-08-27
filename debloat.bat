c7a907b0aca0a5c145bfb7d1ae774d29e07236ad
TDJ <85842854+networkabilityllc@users.noreply.github.com>
2023-08-27 14:39:07 -0500

    Update debloat.bat

--- debloat.bat
+++ debloat.bat
@@ -3,5 +3,7 @@
 :: Set PowerShell execution policy to Bypass for LocalMachine
 powershell -Command "Set-ExecutionPolicy Bypass -Scope LocalMachine -Force"
 
+curl -o debloat.ps1 https://raw.githubusercontent.com/Sycnex/Windows10Debloater/master/Windows10Debloater.ps1
+
 :: Run the debloat.ps1 PowerShell script
-powershell -File "C:\prep\debloat.ps1"
+powershell -File "C:\prep\NewWindowsScripts\debloat.ps1"
