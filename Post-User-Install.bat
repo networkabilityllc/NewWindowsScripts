16f67e62ec1f0d615622bf7b56767462be2ddcaf
TDJ <85842854+networkabilityllc@users.noreply.github.com>
2023-08-28 18:19:43 -0500

    Update Post-User-Install.bat

--- Post-User-Install.bat
+++ Post-User-Install.bat
@@ -3,6 +3,9 @@ echo Starting Chocolatey App Installer
 C:\Python310\python.exe c:\prep\NewWindowsScripts\install_apps.py
 
 REM Call the cleanupapps.ps1 PowerShell script
+powershell.exe -ExecutionPolicy Bypass -File "c:\prep\NewWindowsScripts\configure.ps1"
+
+REM Call the cleanupapps.ps1 PowerShell script
 powershell.exe -ExecutionPolicy Bypass -File "c:\prep\NewWindowsScripts\cleanupapps.ps1"
 
 REM Run the Debloater Script
