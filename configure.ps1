cf70e6e930653ea44d0f9736895d12b0eb1b9ed2
TDJ <85842854+networkabilityllc@users.noreply.github.com>
2023-08-28 17:02:32 -0500

    Add Shortcut for New Users

--- configure.ps1
+++ configure.ps1
@@ -14,3 +14,22 @@ reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" 
 reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" /v SilentInstalledAppsEnabled /d 0 /t REG_DWORD /f
 reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager\" /v SystemPaneSuggestionsEnabled /d 0 /t REG_DWORD /f
 reg.exe add "HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" /f /ve
+
+# Set the paths
+$shortcutPath = "C:\Users\Default\Desktop\Run After User Setup.lnk"
+$targetPath = "C:\prep\NewWindowsScripts\post-user-install.bat"
+
+# Create the WScript Shell Object
+$WshShell = New-Object -comObject WScript.Shell
+
+# Create the shortcut
+$Shortcut = $WshShell.CreateShortcut($shortcutPath)
+$Shortcut.TargetPath = $targetPath
+
+# Additional optional settings
+$Shortcut.Description = "Shortcut to Post-User-Install Script"
+$Shortcut.IconLocation = "$env:SystemRoot\System32\imageres.dll,2" # Example icon, you can change this
+
+# Save the shortcut
+$Shortcut.Save()
+
