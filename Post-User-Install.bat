9e8893060353d3b94b5c75080f1b5137f6aa61ad
TDJ <85842854+networkabilityllc@users.noreply.github.com>
2023-08-30 15:45:58 -0500

    Update Post-User-Install.bat

--- Post-User-Install.bat
+++ Post-User-Install.bat
@@ -8,6 +8,9 @@ powershell.exe -ExecutionPolicy Bypass -File "c:\prep\NewWindowsScripts\configur
 REM Call the cleanupapps.ps1 PowerShell script
 powershell.exe -ExecutionPolicy Bypass -File "c:\prep\NewWindowsScripts\cleanupapps.ps1"
 
+REM Download Windows 10 Debloater Script
+powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/Sycnex/Windows10Debloater/master/Windows10Debloater.ps1' -OutFile 'C:\prep\NewWindowsScripts\debloat.ps1'"
+
 REM Run the Debloater Script
 cd /d "C:\prep\NewWindowsScripts"
 powershell -ExecutionPolicy Bypass -File "debloat.ps1"
