5bc2df754560367b1c391dab0ea548dce577a5a5
TDJ <85842854+networkabilityllc@users.noreply.github.com>
2023-08-30 15:43:19 -0500

    Update Post-User-Install.bat

--- Post-User-Install.bat
+++ Post-User-Install.bat
@@ -9,7 +9,9 @@ REM Call the cleanupapps.ps1 PowerShell script
 powershell.exe -ExecutionPolicy Bypass -File "c:\prep\NewWindowsScripts\cleanupapps.ps1"
 
 REM Run the Debloater Script
-powershell -ExecutionPolicy Bypass -File "C:\prep\NewWindowsScripts\debloat.ps1"
+cd /d "C:\prep\NewWindowsScripts"
+powershell -ExecutionPolicy Bypass -File "debloat.ps1"
+
 
 REM Pause to keep the command prompt window open
 pause
