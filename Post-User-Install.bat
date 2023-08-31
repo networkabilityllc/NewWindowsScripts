e8f48b08e89ea755adeba4751f69cec181a39836
TDJ <85842854+networkabilityllc@users.noreply.github.com>
2023-08-31 14:40:49 -0500

    Update Post-User-Install.bat

--- Post-User-Install.bat
+++ Post-User-Install.bat
@@ -1,14 +1,15 @@
 @echo off
 
 REM Invoke PowerShell to run the script from the provided URL
-powershell.exe -ExecutionPolicy Bypass -Command "iwr -useb https://raw.githubusercontent.com/networkabilityllc/NewWindowsScripts/main/workstationprep.ps1 | iex"
+REM powershell.exe -ExecutionPolicy Bypass -Command "iwr -useb https://raw.githubusercontent.com/networkabilityllc/NewWindowsScripts/main/workstationprep.ps1 | iex"
+powershell.exe -ExecutionPolicy Bypass -Command "iwr -useb https://raw.githubusercontent.com/networkabilityllc/NewWindowsScripts/main/configure.ps1 | iex"
 
 REM Run the Choco Installer using Python
 echo Starting Chocolatey App Installer
 C:\Python310\python.exe c:\prep\NewWindowsScripts\install_apps.py
 
 REM Call the configure.ps1 PowerShell script
-powershell.exe -ExecutionPolicy Bypass -File "c:\prep\NewWindowsScripts\configure.ps1"
+REM powershell.exe -ExecutionPolicy Bypass -File "c:\prep\NewWindowsScripts\configure.ps1"
 
 REM Call the cleanupapps.ps1 PowerShell script
 powershell.exe -ExecutionPolicy Bypass -File "c:\prep\NewWindowsScripts\cleanupapps.ps1"
