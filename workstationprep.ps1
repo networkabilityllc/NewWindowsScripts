5f346727a7acbfd7f4225fed66e145ce6edb051e
TDJ <85842854+networkabilityllc@users.noreply.github.com>
2023-09-10 14:01:33 -0500

    Remove Development Branch

--- workstationprep.ps1
+++ workstationprep.ps1
@@ -277,7 +277,7 @@ $gitPath = "C:\Program Files\Git\bin\git.exe"
 $repoPath = "c:\prep\NewWindowsScripts"
 if (-not (Test-Path -Path $repoPath)) {
     # Clone the GitHub repository
-    $gitRepoUrl = "https://github.com/networkabilityllc/NewWindowsScripts/Development"
+    $gitRepoUrl = "https://github.com/networkabilityllc/NewWindowsScripts"
     Start-Process -FilePath $gitPath -ArgumentList "clone", $gitRepoUrl, $repoPath
 } else {
 	# ------------------------------------------------------------
