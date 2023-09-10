27ac15fda235a9029c7e85149051e89ed0fc4392
TDJ <85842854+networkabilityllc@users.noreply.github.com>
2023-09-10 16:25:21 -0500

    Toggle UAC Script Added

--- Post-User-Install.bat
+++ Post-User-Install.bat
@@ -10,5 +10,8 @@ C:\Python310\python.exe c:\prep\NewWindowsScripts\install_apps.py
 REM Call the cleanupapps.ps1 PowerShell script
 powershell.exe -ExecutionPolicy Bypass -File "c:\prep\NewWindowsScripts\cleanupapps.ps1"
 
+REM Call the Toggle UAC PowerShell script
+powershell.exe -ExecutionPolicy Bypass -File "c:\prep\NewWindowsScripts\toggle-uac.ps1"
+
 REM Pause to keep the command prompt window open
 pause
--- configure.ps1
+++ configure.ps1
@@ -234,16 +234,3 @@ Import-StartLayout -LayoutPath $layoutFile -MountPath $env:SystemDrive\
 
 Remove-Item $layoutFile
 
-# Load the System.Windows.Forms assembly
-Add-Type -AssemblyName System.Windows.Forms
-
-# Function to toggle UAC
-# Display a dialog box to ask if the user wants to re-enable UAC
-$dialogResult = [System.Windows.Forms.MessageBox]::Show("Do you want to re-enable UAC?", "UAC Re-enable", [System.Windows.Forms.MessageBoxButtons]::YesNo, [System.Windows.Forms.MessageBoxIcon]::Question)
-
-if ($dialogResult -eq [System.Windows.Forms.DialogResult]::Yes) {
-    Enable-UAC
-    Write-Host "UAC has been re-enabled."
-} else {
-    Write-Host "UAC remains disabled."
-}
