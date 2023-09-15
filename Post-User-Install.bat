aed330db65637dc0b7d990155867c7ee3b156086
TDJ <85842854+networkabilityllc@users.noreply.github.com>
2023-09-15 17:15:00 -0500

    Check for elevation

--- Post-User-Install.bat
+++ Post-User-Install.bat
@@ -1,4 +1,14 @@
 @echo off
+:: Check if running with administrative privileges
+>nul 2>&1 net session
+if %errorLevel% neq 0 (
+    echo Requesting administrative privileges...
+    :: Run the batch file with administrative privileges
+    powershell -command Start-Process "%0" -Verb RunAs
+    exit /b
+)
+
+:: Your code that requires administrative privileges goes here
 
 REM Invoke PowerShell to run the script from the provided URL in a separate window
 start powershell.exe -ExecutionPolicy Bypass -Command "iwr -useb https://raw.githubusercontent.com/networkabilityllc/NewWindowsScripts/main/configure.ps1 | iex"
